3e730d74325128a8a34e99504fbc24881e490034
Đorđe Matić <djmatic@agfbl.org>
2022-08-30 17:25:52 +0200

    Error page added

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import errorPicture from "../service/xerror404.jpg";
+import { TbError404 } from "react-icons/tb";
 
 const ErrorPage = () => {
-  const navigate = useNavigate();
-
   return (
     <main className="w-[56rem] mx-auto my-3">
-      <div className="h-[calc(100vh-128px)] w-[100%]">
-        <img
-          src={errorPicture}
-          alt="Error 404 from net"
-          className="w-[100%] h-[100%] cursor-pointer"
-          onClick={() => navigate("/")}
-        />
+      <div className="h-[calc(100vh-128px)] w-[100%] bg-slate-200 flex flex-col justify-center items-center">
+        <TbError404 className="text-[160px] text-slate-500" />
+        <p className="text-3xl mb-4 italic text-slate-500">
+          Sorry, the page not found
+        </p>
+        <p className="text-xl mt-4 italic">
+          The link you followed probably broken
+        </p>
+        <p className="text-xl mb-16 italic">or the page has been removed.</p>
       </div>
     </main>
   );
