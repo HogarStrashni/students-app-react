f71da8377dc0d4b8de75468368f5f663c9b70520
Đorđe Matić <djmatic@agfbl.org>
2022-09-10 10:10:29 +0200

    add new logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../service/logo.png";
+import logo from "../service/logo.jpg";
 import { useLocation, useNavigate } from "react-router-dom";
 import { FaSignInAlt } from "react-icons/fa";
 import { useAuth } from "../context";
@@ -23,41 +23,49 @@ const Header = () => {
   };
 
   return (
-    <header className="h-16 px-[4%] mx-auto flex justify-between text-gray-500 border-b-2">
+    <header className="h-16 px-[4%] flex justify-between text-gray-500 border-b-2">
       <Toaster />
-      <div className="flex justify-between items-center">
-        <a href="https://www.mongodb.com/mern-stack" target="_blanc">
+      <div className="h-14 pl-4 pr-6 my-auto flex justify-between items-center rounded-xl shadow-sm">
+        <a
+          href="https://www.mongodb.com/mern-stack"
+          target="_blanc"
+          className="flex"
+        >
           <img
             src={logo}
             alt="logo"
-            className="h-12 rounded-lg cursor-pointer"
+            className="h-14 w-14 rounded-lg cursor-pointer"
           />{" "}
+          <div className="ml-2 text-[#39B54A] pt-0.5">
+            <h2 className="text-2xl">
+              <span className="font-extrabold">ONLINE</span> STUDY
+            </h2>
+            <h3 className="text-xs text-center font-medium">
+              NO STUCK WITH MERN STACK
+            </h3>
+          </div>
         </a>
-
-        <div className="w-96 ml-12 flex justify-between items-center text-sm font-medium text-gray-500">
-          <p onClick={() => navigate("/")} className={`${hoverLink.header}`}>
-            Home
-          </p>
-          <a
-            href="https://www.unibl.org/"
-            target="_blanc"
-            className={`${hoverLink.header}`}
-          >
-            University
-          </a>
-          <p
-            onClick={() => navigate("/documentation")}
-            className={`${hoverLink.header}`}
-          >
-            Documentation
-          </p>
-          <p
-            onClick={() => navigate("/about")}
-            className={`${hoverLink.header}`}
-          >
-            About Us
-          </p>
-        </div>
+      </div>
+      <div className="w-96 ml-8 flex justify-between items-center text-sm font-medium text-gray-500">
+        <p onClick={() => navigate("/")} className={`${hoverLink.header}`}>
+          Home
+        </p>
+        <a
+          href="https://www.unibl.org/"
+          target="_blanc"
+          className={`${hoverLink.header}`}
+        >
+          University
+        </a>
+        <p
+          onClick={() => navigate("/documentation")}
+          className={`${hoverLink.header}`}
+        >
+          Documentation
+        </p>
+        <p onClick={() => navigate("/about")} className={`${hoverLink.header}`}>
+          About Us
+        </p>
       </div>
       {!loggedInUser ? (
         !(
@@ -65,7 +73,7 @@ const Header = () => {
         ) &&
         !loggedInUser && (
           <button
-            className="px-5 my-3 shadow-sm flex items-center text-sm font-medium text-gray-50 bg-blue-500 hover:text-white border border-blue-500 hover:bg-blue-800 rounded-lg"
+            className="px-5 ml-auto my-3 shadow-sm flex items-center text-sm font-medium text-gray-50 bg-blue-500 hover:text-white border border-blue-500 hover:bg-blue-800 rounded-lg"
             onClick={() => navigate("/login")}
           >
             <FaSignInAlt className="mr-1 text-inherit" />
